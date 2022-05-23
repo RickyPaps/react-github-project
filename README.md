557ed9d3308b6f5d14e4666db6e8896e68e7162b
Riccardo Papini <rickypapini@gmail.com>
2022-05-23 18:18:41 +0200

    Handled redesign of site.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Getting Started with Create React App
+** This project uses React & Typescript with Mantine UI framework. GraphQL with Apollo for the API calls. **
 
-This project uses React & Typescript with Mantine UI framework. GraphQL with Apollo for the API calls.
+    - This project utilizes the Githib GraphQL API to retrieve information about up to date issues that are logged on their site.
+
+    - The user can currently view opened and closed issues in the reactjs github repo.
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,15 +3,12 @@ import { Modal } from "@mantine/core";
 import { Comment } from "../Comment/Comment";
 import { modalDropIn } from "../../animations/animation";
 
-
 export const RepoModal = ({ selectedModal, opened, setOpened }: any) => {
   return (
     <motion.div variants={modalDropIn} initial="hidden" animate="visible">
       <Modal
+        overflow="inside"
         size="calc(100vw - 87px)"
-        transition="fade"
-        transitionDuration={600}
-        transitionTimingFunction="ease"
         opened={opened}
         onClose={() => setOpened(false)}
         title={selectedModal ? selectedModal.node.title : "Github Issue"}
